b7e3fc30fa5756feeee9cc2380ea63b0832d6725
David Bodunde <davidbodunde0@gmail.com>
2024-07-19 12:45:58 +0100

    jul 19

--- src/app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardHome() {
-    return (
-      <div>
-        <h1 className="text-2xl font-bold mb-4">Welcome to your Dashboard</h1>
-        {/* Add your dashboard content here */}
-      </div>
-    );
-  }
